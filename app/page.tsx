742204017c686a4106741fdfd16f82c69d340059
brahim <ibrahim.elmliji01@gamil.com>
2024-01-26 17:57:33 +0100

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Hero from './components/main/Hero';
 
 const page = () => {
   return (
     <main className='w-full h-full'>
-      <div className='flex flex-col gap-20'>
-
+      <div className='flex flex-col gap-20 h-[850px]'>
+        <Hero />
       </div>
     </main>
   )
