fd2502a981b823ddf845c122feebe5e2a744382a
brahim <ibrahim.elmliji01@gamil.com>
2024-01-26 18:30:54 +0100

    getting the first animation

--- app/components/main/Hero.tsx
+++ app/components/main/Hero.tsx
@@ -1,3 +1,5 @@
+import HeroContent from "../sub/HeroContent";
+
 const Hero = () => {
   return (
     <div className="relative flex flex-col w-full">
@@ -9,6 +11,7 @@ const Hero = () => {
       >
         <source src="/blackhole.webm" type="video/webm" />
       </video>
+      <HeroContent />
     </div>
   );
 };
--- app/components/sub/HeroContent.tsx
+++ app/components/sub/HeroContent.tsx
@@ -1,6 +1,8 @@
+"use client"
 import React from 'react'
 import {motion} from 'framer-motion';
 import { slideInFromTop } from '@/utils/motion';
+import { SparklesIcon } from '@heroicons/react/16/solid';
 
 const HeroContent = () => {
   return (
@@ -14,7 +16,8 @@ const HeroContent = () => {
             variants={slideInFromTop}
             className='Welcome-box p-[15px] px-[4px] border-[#7042f88b] opacity-[0.9]'
             >
-
+              <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5' />
+              <h1 className='welcome-text text-[13px]'>Hello Im brahim Elmliji a Fullstack Developer </h1>
             </motion.div>
         </div>
     </motion.div>
